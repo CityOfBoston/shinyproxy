cd380f51b9bc021808c0440f500dc5e4d6cdf8f5
luis sano <luissano@gmail.com>
2017-04-26 16:34:28 -0400

    things

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-source /tmp/shiny_proxy_ip && \
-(nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP '(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill)' &)  && \
-ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'cd ~/shinyproxy/ && (nohup java -jar shinyproxy-0.8.7.jar &)'
 
-
-#&& ( cd ~/shinyproxy/ && nohup java -jar shinyproxy-0.8.7.jar &)'  ||
+source /tmp/shiny_proxy_ip && ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP '(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill)'
+source /tmp/shiny_proxy_ip && ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'cd ~/shinyproxy/ && (nohup java -jar shinyproxy-0.8.7.jar &)'
