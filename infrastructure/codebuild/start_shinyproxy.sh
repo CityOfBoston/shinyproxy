966d6e05339fcc0c9699dc2c8f4ee8b0b799bd1c
luis sano <luissano@gmail.com>
2017-04-26 17:34:08 -0400

    nohup everywhere

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -2,4 +2,4 @@
 
 
 source /tmp/shiny_proxy_ip && ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP '(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill)'
-source /tmp/shiny_proxy_ip && ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'cd ~/shinyproxy/ && (nohup java -jar shinyproxy-0.8.7.jar &)'
+nohup source /tmp/shiny_proxy_ip && ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'nohup cd ~/shinyproxy/ && (java -jar shinyproxy-0.8.7.jar ) &' &
