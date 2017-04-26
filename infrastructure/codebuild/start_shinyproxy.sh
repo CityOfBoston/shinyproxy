e0322109bfe9ef447ea5fdab4d385bb25de54169
luis sano <luissano@gmail.com>
2017-04-26 15:31:01 -0400

    lets try this crazy command

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -2,5 +2,5 @@
 
 
 source /tmp/shiny_proxy_ip && \
-ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP 'kill $(jps -ml | grep /home/ubuntu/shinyproxy/shinyproxy-0.8.7.jar | grep -P -o "\d+\s" | awk "{print $1}")'  && \
+ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP '(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill) && java -jar ~/shinyproxy/shinyproxy-0.8.7.jar'  || \
 nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'java -jar ~/shinyproxy/shinyproxy-0.8.7.jar' &
