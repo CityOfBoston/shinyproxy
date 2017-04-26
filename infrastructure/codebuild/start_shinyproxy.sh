e460f6ff1d72b43a12e5a16f2179c380e5e769ae
luis sano <luissano@gmail.com>
2017-04-26 15:35:04 -0400

    cant forgot the nohup

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -2,5 +2,5 @@
 
 
 source /tmp/shiny_proxy_ip && \
-ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP '(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill) && java -jar ~/shinyproxy/shinyproxy-0.8.7.jar'  || \
+ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP '(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill) && nohup java -jar ~/shinyproxy/shinyproxy-0.8.7.jar &'  || \
 nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'java -jar ~/shinyproxy/shinyproxy-0.8.7.jar' &
