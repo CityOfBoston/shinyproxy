0260abaedc84262af3914bc47198ebb4d578c7f0
luis sano <luissano@gmail.com>
2017-04-26 15:11:11 -0400

    kill shiny proxy before starting a new one

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+
 source /tmp/shiny_proxy_ip && \
-nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  $repo ubuntu@$SHINY_PROXY_IP  'java -jar ~/shinyproxy/shinyproxy-0.8.7.jar' &
+ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@SHINY_PROXY_IP 'kill $(jps -ml | grep /home/ubuntu/shinyproxy/shinyproxy-0.8.7.jar | grep -P -o "\d+\s" | awk "{print $1}")'  && \
+nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'java -jar ~/shinyproxy/shinyproxy-0.8.7.jar' &
