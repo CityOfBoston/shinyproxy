5be2411bfdb711192b9ada9535b40fa3f83d557a
luis sano <luissano@gmail.com>
2017-04-26 18:00:00 -0400

    added some logging

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-
+echo "going to kill any shinyproxy processes before starting up a new one"
 source /tmp/shiny_proxy_ip && (nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP '(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill)' &)
+echo "starting up a new shinyproxy process"
 source /tmp/shiny_proxy_ip && (nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'nohup cd ~/shinyproxy/ && (java -jar shinyproxy-0.8.7.jar ) &' &)
+echo "done starting shinyproxy" 
