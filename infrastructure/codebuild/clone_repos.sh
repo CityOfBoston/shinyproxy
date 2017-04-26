ab6149a06ef82f2542e003cca341fa41f5e1c53c
luis sano <luissano@gmail.com>
2017-04-26 14:11:46 -0400

    added message

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -4,6 +4,7 @@ mkdir shinyapps
 cd shinyapps
 source /tmp/shiny_proxy_ip && \
 while read repo; do
+    echo "cloning from ${repo}"
     git clone $repo &&  \
     scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $repo ubuntu@$SHINY_PROXY_IP && \
     ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $repo ubuntu@$SHINY_PROXY_IP /bin/bash -c  "cd $repo && docker build"
