6f75c23d11b45c7efadd0ead0b3d7671131f63d4
luis sano <luissano@gmail.com>
2017-04-26 14:49:14 -0400

    proper remote commands

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source /tmp/shiny_proxy_ip && \
-ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  $repo ubuntu@$SHINY_PROXY_IP /bin/bash -c  "java -jar ~/shinyproxy/shinyproxy-0.8.7.jar"
+nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  $repo ubuntu@$SHINY_PROXY_IP  'java -jar ~/shinyproxy/shinyproxy-0.8.7.jar' &
