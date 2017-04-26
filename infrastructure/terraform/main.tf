e01ea13f85da3f86ef412e191d88bd8307dfaad4
luis sano <luissano@gmail.com>
2017-04-26 10:57:19 -0400

    spelling change and sourcing from github rather than locally

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -7,7 +7,8 @@ provider "aws" {
 
 
 module "aws_vpc" {
-  source = "../../../boston_analytics_tf_modules/vpc"
+  #source = "../../../boston_analytics_tf_modules/vpc"
+  source = "git@github.com:CityOfBoston/boston_analytics_tf_modules.git//vpc?ref=v0.0.4.2"
   name = "shiny-proxy-vpc"
 
   cidr = "10.0.0.0/16"
