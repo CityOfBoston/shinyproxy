6673d5c2abecfbf72575ad49c5ea771aaa3d13b1
luis sano <luissano@gmail.com>
2017-04-27 11:43:40 -0400

    this script isnt working why is that

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -2,13 +2,15 @@
 export ROOT=$PWD
 mkdir shinyapps
 cd shinyapps
-source /tmp/shiny_proxy_ip && \
+source /tmp/shiny_proxy_ip
 while read repo; do
     echo "cloning from ${repo}"
-    git clone $repo &&  \
-    scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $repo ubuntu@$SHINY_PROXY_IP:~/ && \
-    export IMAGE_TAG=$(echo $repo | grep -P -o "^\w+") && \
-    ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no $repo ubuntu@$SHINY_PROXY_IP /bin/bash -c  "cd $repo && git checkout docker && docker build -t ${IMAGE_TAG} ."
+    git clone $repo
+    echo "moving files to shiny server"
+    scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $ROOT/$repo ubuntu@$SHINY_PROXY_IP:~/
+    export IMAGE_TAG=$(echo $repo | grep -P -o "^\w+")
+    echo "building docker images on shiny server"
+    ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP /bin/bash -c  "cd $repo && git checkout docker && docker build -t ${IMAGE_TAG} ."
 done < $ROOT/repositories.conf
 
 
