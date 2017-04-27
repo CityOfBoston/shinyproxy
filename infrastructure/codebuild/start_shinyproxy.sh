ca8ecce38971d11c1fe6e790d9bcc55150072c72
luis sano <luissano@gmail.com>
2017-04-27 16:10:48 -0400

    i think tihs might fix the not updating application problem

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -4,8 +4,9 @@ source /tmp/shiny_proxy_ip
 scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no application.yml ubuntu@$SHINY_PROXY_IP:~/shinyproxy/application.yml
 ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << EOF
 echo "going to kill any shinyproxy processes before starting up a new one"
+cd ~/shinyproxy
 echo "(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill) && \
- (cd ~/shinyproxy/ &&  java -jar shinyproxy-0.8.7.jar) || (cd ~/shinyproxy/ && java -jar shinyproxy-0.8.7.jar )" > /tmp/start_proxy.sh
+ (java -jar shinyproxy-0.8.7.jar) || (java -jar shinyproxy-0.8.7.jar )" > /tmp/start_proxy.sh
  chmod u+x /tmp/start_proxy.sh
  nohup ./tmp/start_proxy.sh &
 EOF
