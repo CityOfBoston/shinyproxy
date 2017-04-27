539335a05316600dd78c8e61ac0da76ab4a8abe4
luis sano <luissano@gmail.com>
2017-04-27 12:20:16 -0400

    proper heredoc

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -3,6 +3,7 @@ export ROOT=$PWD
 mkdir shinyapps
 cd shinyapps
 source /tmp/shiny_proxy_ip
+
 echo "attempting clone repos and build docker images"
 while read repo; do
     echo "cloning from ${repo}"
@@ -11,8 +12,10 @@ while read repo; do
     scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $ROOT/$repo ubuntu@$SHINY_PROXY_IP:~/
     export IMAGE_TAG=$(echo $repo | grep -P -o "^\w+")
     echo "building docker images on shiny server"
-    ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@$SHINY_PROXY_IP << EOF
-        cd $repo && git checkout docker && docker build -t ${IMAGE_TAG} .
-    EOF
-
+    ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
+        cd $repo
+        git checkout docker && docker build -t ${IMAGE_TAG} .
+EOF
 done < $ROOT/repositories.conf
+
+
