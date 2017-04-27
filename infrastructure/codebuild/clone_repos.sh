0071294e2d72f1f7a6997337a32730c1bccdb2ba
luis sano <luissano@gmail.com>
2017-04-27 15:11:49 -0400

    okay misspelling

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -33,7 +33,7 @@ scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $NAME ubuntu@$SHINY_
 ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
         cd ~/shinyproxy/$NAME
         git checkout docker
-        sudo docker build -t ${NAME} .
+        sudo docker build -t bostonanalytics/${NAME} .
 EOF
 echo "done installing docker images for repos"
 
