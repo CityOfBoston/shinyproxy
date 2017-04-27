9789649b5c40aa688053b03ee8b1dd72afbeaf7b
luis sano <luissano@gmail.com>
2017-04-27 12:26:26 -0400

    pterminal

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -9,10 +9,10 @@ while read repo; do
     echo "cloning from ${repo}"
     git clone $repo
     echo "moving files to shiny server"
-    scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $ROOT/$repo ubuntu@$SHINY_PROXY_IP:~/
+    scp -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $ROOT/$repo ubuntu@$SHINY_PROXY_IP:~/
     export IMAGE_TAG=$(echo $repo | grep -P -o "^\w+")
     echo "building docker images on shiny server"
-    ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
+    ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
         cd $repo
         git checkout docker && docker build -t ${IMAGE_TAG} .
 EOF
