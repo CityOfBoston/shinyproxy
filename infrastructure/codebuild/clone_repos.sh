a91f89427f109661bdc760e1dcf617448837d4d7
luis sano <luissano@gmail.com>
2017-04-27 08:54:01 -0400

    lets see if this builds the approp image

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -7,7 +7,8 @@ while read repo; do
     echo "cloning from ${repo}"
     git clone $repo &&  \
     scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $repo ubuntu@$SHINY_PROXY_IP && \
-    ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no $repo ubuntu@$SHINY_PROXY_IP /bin/bash -c  "cd $repo && docker build"
+    export IMAGE_TAG=$(echo $repo | grep -P -o "^\w+") && \
+    ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no $repo ubuntu@$SHINY_PROXY_IP /bin/bash -c  "cd $repo && git checkout docker && docker build -t ${IMAGE_TAG} ."
 done < $ROOT/repositories.conf
 
 
--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -4,4 +4,4 @@ echo "going to kill any shinyproxy processes before starting up a new one"
 source /tmp/shiny_proxy_ip && (nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP '(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill)' &)
 echo "starting up a new shinyproxy process"
 source /tmp/shiny_proxy_ip && (nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'nohup cd ~/shinyproxy/ && (java -jar shinyproxy-0.8.7.jar ) &' &)
-echo "done starting shinyproxy" 
+echo "done starting shinyproxy"
