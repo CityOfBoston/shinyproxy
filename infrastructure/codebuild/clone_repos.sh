532a4cb10dde2a5fba3000fead355cedf0be80d4
luis sano <luissano@gmail.com>
2017-04-27 11:54:13 -0400

    well lets see where we are

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -3,14 +3,15 @@ export ROOT=$PWD
 mkdir shinyapps
 cd shinyapps
 source /tmp/shiny_proxy_ip
+echo "attempting clone repos and build docker images"
 while read repo; do
     echo "cloning from ${repo}"
     git clone $repo
     echo "moving files to shiny server"
-    scp -i -t -t  ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $ROOT/$repo ubuntu@$SHINY_PROXY_IP:~/
+    scp -i -t  ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $ROOT/$repo ubuntu@$SHINY_PROXY_IP:~/
     export IMAGE_TAG=$(echo $repo | grep -P -o "^\w+")
     echo "building docker images on shiny server"
-    ssh -t -t -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP /bin/bash -c  "cd $repo && git checkout docker && docker build -t ${IMAGE_TAG} ."
+    ssh -t -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP /bin/bash -c  "cd $repo && git checkout docker && docker build -t ${IMAGE_TAG} ."
 done < $ROOT/repositories.conf
 
 
