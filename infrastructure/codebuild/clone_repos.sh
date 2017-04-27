021fa4c2935f114894e70fec4b3205c0f8e12604
luis sano <luissano@gmail.com>
2017-04-27 11:26:34 -0400

    how does this nohup keep getting ignored

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -6,7 +6,7 @@ source /tmp/shiny_proxy_ip && \
 while read repo; do
     echo "cloning from ${repo}"
     git clone $repo &&  \
-    scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $repo ubuntu@$SHINY_PROXY_IP && \
+    scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $repo ubuntu@$SHINY_PROXY_IP:~/ && \
     export IMAGE_TAG=$(echo $repo | grep -P -o "^\w+") && \
     ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no $repo ubuntu@$SHINY_PROXY_IP /bin/bash -c  "cd $repo && git checkout docker && docker build -t ${IMAGE_TAG} ."
 done < $ROOT/repositories.conf
--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -8,8 +8,8 @@ source /tmp/shiny_proxy_ip
 #(nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'nohup cd ~/shinyproxy/ && (java -jar shinyproxy-0.8.7.jar ) &' &)
 #echo "done starting shinyproxy"
 
-ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << EOF
+nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << EOF
 echo "going to kill any shinyproxy processes before starting up a new one"
 (jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill) && \
  (cd ~/shinyproxy/ &&  nohup java -jar shinyproxy-0.8.7.jar &) || (cd ~/shinyproxy/ && nohup java -jar shinyproxy-0.8.7.jar &)
-EOF
+EOF &
