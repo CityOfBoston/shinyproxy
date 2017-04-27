363c7a7760b588fcd3d2a4c8fade51ee3113ebc0
luis sano <luissano@gmail.com>
2017-04-27 11:36:25 -0400

    okay this is easier

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 source /tmp/shiny_proxy_ip
 
-#echo "going to kill any shinyproxy processes before starting up a new one"
-# (ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP '(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill)') && \
-# (nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'nohup cd ~/shinyproxy/ && (java -jar shinyproxy-0.8.7.jar ) &' &) || \
-#echo "starting up a new shinyproxy process"
-#(nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP  'nohup cd ~/shinyproxy/ && (java -jar shinyproxy-0.8.7.jar ) &' &)
-#echo "done starting shinyproxy"
-
-nohup ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << EOF
+ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << EOF
 echo "going to kill any shinyproxy processes before starting up a new one"
-(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill) && \
- (cd ~/shinyproxy/ &&  nohup java -jar shinyproxy-0.8.7.jar &) || (cd ~/shinyproxy/ && nohup java -jar shinyproxy-0.8.7.jar &)
-EOF &
+echo "(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill) && \
+ (cd ~/shinyproxy/ &&  java -jar shinyproxy-0.8.7.jar) || (cd ~/shinyproxy/ && java -jar shinyproxy-0.8.7.jar )" > /tmp/start_proxy.sh
+ chmod u+x /tmp/start_proxy.sh
+ nohup ./tmp/start_proxy.sh &
+EOF
