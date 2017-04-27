1fe1e0cc5d0cb3a1a384975432b2472ebcfaceb2
luis sano <luissano@gmail.com>
2017-04-27 13:25:12 -0400

    okay getting closer i think

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -8,11 +8,11 @@ echo "attempting clone repos and build docker images from the following list"
 cat $ROOT/repositories.conf
 while read repo; do
     export REPO_NAME=$(echo $repo | grep -P -o "git@github.com:CityOfBoston\/\w+.git")
+    export IMAGE_TAG=$(echo $repo | grep -P -o "^\w+")
     echo "cloning from ${REPO_NAME}"
     git clone $REPO_NAME
     echo "moving files to shiny server"
-    scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $REPO_NAME ubuntu@$SHINY_PROXY_IP:~/shinyproxy/
-    export IMAGE_TAG=$(echo $repo | grep -P -o "^\w+")
+    scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $IMAGE_TAG ubuntu@$SHINY_PROXY_IP:~/shinyproxy/
     echo "building docker images on shiny server"
     ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
         cd ~/shinyproxy/$REPO_NAME
