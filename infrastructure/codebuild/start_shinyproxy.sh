f3ad97203b10d9e61612196d21897e5845111b95
luis sano <luissano@gmail.com>
2017-04-28 11:23:23 -0400

    disable EOF quoting

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -2,7 +2,7 @@
 source /tmp/shiny_proxy_ip
 
 scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no application.yml ubuntu@$SHINY_PROXY_IP:~/shinyproxy/application.yml
-ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << EOF
+ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << 'EOF'
 echo "going to kill any shinyproxy processes before starting up a new one"
 cd ~/shinyproxy
 echo "(jps -ml | grep shinyproxy | grep -P -o \"\\d+\\s\" | awk \"{print '\$1'}\" | xargs kill) || echo \"nothing currently running\"; java -jar shinyproxy-0.8.7.jar" > /tmp/start_proxy.sh
