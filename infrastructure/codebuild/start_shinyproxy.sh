e968f7b399432a480342d59e392e3761df9b623d
luis sano <luissano@gmail.com>
2017-04-28 10:07:08 -0400

    well that was a dumb mistake

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -7,5 +7,5 @@ echo "going to kill any shinyproxy processes before starting up a new one"
 cd ~/shinyproxy
 echo "(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill) && java -jar shinyproxy-0.8.7.jar || (java -jar shinyproxy-0.8.7.jar )" > /tmp/start_proxy.sh
  chmod u+x /tmp/start_proxy.sh
- nohup ./tmp/start_proxy.sh &
+ nohup /tmp/start_proxy.sh &
 EOF
