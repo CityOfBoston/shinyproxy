5ad818915773e73760492b8b883210ff7c3a1c86
luis sano <luissano@gmail.com>
2017-04-28 16:10:35 -0400

    building on master from now on

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -18,26 +18,10 @@ EOF
     scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $NAME ubuntu@$SHINY_PROXY_IP:~/shinyproxy/
     ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
             cd ~/shinyproxy/$NAME
-            git checkout docker
             sudo docker build -t bostonanalytics/${NAME} .
 EOF
 done < $ROOT/repositories.conf
 
-##export repo=$(cat $ROOT/repositories.conf)
-#export REPO_NAME=$(echo $repo | grep -P -o "git@github.com:CityOfBoston\/\w+.git")
-#export NAME=$(echo $repo | grep -P -o "^\w+")
-#git clone $REPO_NAME
-#
-#ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
-# sudo rm -rf ~/shinyproxy/$NAME || echo "nothing here so nothing to delete"
-#EOF
-#
-#scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $NAME ubuntu@$SHINY_PROXY_IP:~/shinyproxy/
-#ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
-#        cd ~/shinyproxy/$NAME
-#        git checkout docker
-#        sudo docker build -t bostonanalytics/${NAME} .
-#EOF
 echo "done building docker images for shiny apps"
 
 
