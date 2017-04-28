0a28645d2793244c791584de545fc8593887e020
luis sano <luissano@gmail.com>
2017-04-28 16:03:46 -0400

    using the actual looping structure

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -6,36 +6,39 @@ source /tmp/shiny_proxy_ip
 
 echo "attempting clone repos and build docker images from the following list"
 cat $ROOT/repositories.conf
-#while read repo; do
-#    export REPO_NAME=$(echo $repo | grep -P -o "git@github.com:CityOfBoston\/\w+.git")
-#    export IMAGE_TAG=$(echo $repo | grep -P -o "^\w+")
-#    echo "cloning from ${REPO_NAME}"
-#    git clone $REPO_NAME
-#    echo "moving files to shiny server"
-#    scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $IMAGE_TAG ubuntu@$SHINY_PROXY_IP:~/shinyproxy/
-#    echo "building docker images on shiny server"
-#    ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
-#        cd ~/shinyproxy/$REPO_NAME
-#        git checkout docker && docker build -t ${IMAGE_TAG} .
+while read repo; do
+    export REPO_NAME=$(echo $repo | grep -P -o "git@github.com:CityOfBoston\/\w+.git")
+    export NAME=$(echo $repo | grep -P -o "^\w+")
+    git clone $REPO_NAME
+
+    ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
+     sudo rm -rf ~/shinyproxy/$NAME || echo "nothing here so nothing to delete"
+EOF
+
+    scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $NAME ubuntu@$SHINY_PROXY_IP:~/shinyproxy/
+    ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
+            cd ~/shinyproxy/$NAME
+            git checkout docker
+            sudo docker build -t bostonanalytics/${NAME} .
+EOF
+done < $ROOT/repositories.conf
+
+##export repo=$(cat $ROOT/repositories.conf)
+#export REPO_NAME=$(echo $repo | grep -P -o "git@github.com:CityOfBoston\/\w+.git")
+#export NAME=$(echo $repo | grep -P -o "^\w+")
+#git clone $REPO_NAME
+#
+#ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
+# sudo rm -rf ~/shinyproxy/$NAME || echo "nothing here so nothing to delete"
 #EOF
-#done < $ROOT/repositories.conf
-
-export repo=$(cat $ROOT/repositories.conf)
-export REPO_NAME=$(echo $repo | grep -P -o "git@github.com:CityOfBoston\/\w+.git")
-export NAME=$(echo $repo | grep -P -o "^\w+")
-git clone $REPO_NAME
-
-ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
- sudo rm -rf ~/shinyproxy/$NAME || echo "nothing here so nothing to delete"
-EOF
-
-scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $NAME ubuntu@$SHINY_PROXY_IP:~/shinyproxy/
-ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
-        cd ~/shinyproxy/$NAME
-        git checkout docker
-        sudo docker build -t bostonanalytics/${NAME} .
-EOF
-echo "done installing docker images for repos"
+#
+#scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $NAME ubuntu@$SHINY_PROXY_IP:~/shinyproxy/
+#ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
+#        cd ~/shinyproxy/$NAME
+#        git checkout docker
+#        sudo docker build -t bostonanalytics/${NAME} .
+#EOF
+echo "done building docker images for shiny apps"
 
 
 
