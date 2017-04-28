0d4da4619e5181e0a362ba0ef27b39d2cbf3a648
luis sano <luissano@gmail.com>
2017-04-28 10:14:39 -0400

    redirecting errors

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -5,7 +5,7 @@ scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no application.yml ubuntu@
 ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << EOF
 echo "going to kill any shinyproxy processes before starting up a new one"
 cd ~/shinyproxy
-echo "(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill) && java -jar shinyproxy-0.8.7.jar || (java -jar shinyproxy-0.8.7.jar )" > /tmp/start_proxy.sh
+echo "(jps -ml | grep shinyproxy | grep -P -o "\d+\s" | awk '{print $1}' | xargs kill) && (java -jar shinyproxy-0.8.7.jar) || (java -jar shinyproxy-0.8.7.jar )" > /tmp/start_proxy.sh
  chmod u+x /tmp/start_proxy.sh
- nohup /tmp/start_proxy.sh &
+ nohup /tmp/start_proxy.sh  >/dev/null 2>&1 &
 EOF
