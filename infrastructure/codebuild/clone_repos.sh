70636537709d9e45aef2f34d115bd674d8b8ddbd
luis sano <luissano@gmail.com>
2017-05-10 14:18:19 -0400

    adding some more logs. Not sure if the old repo was getting deleted

--- infrastructure/codebuild/clone_repos.sh
+++ infrastructure/codebuild/clone_repos.sh
@@ -13,14 +13,14 @@ while read repo; do
     export REPO_NAME=$(echo $repo | grep -P -o "git@github.com:CityOfBoston\/\w+.git")
     export NAME=$(echo $repo | grep -P -o "^\w+")
     git clone $REPO_NAME
-
     ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
-     sudo rm -rf ~/shinyproxy/$NAME || echo "nothing here so nothing to delete"
+        sudo rm -rf ~/shinyproxy/$NAME && echo "Deleted old $NAME repo contents" || echo "nothing here so nothing to delete"
 EOF
-
+    echo "copying over $NAME to the shinyproxy server"
     scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no -r $NAME ubuntu@$SHINY_PROXY_IP:~/shinyproxy/
     ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  ubuntu@${SHINY_PROXY_IP} << EOF
             cd ~/shinyproxy/$NAME
+            echo "Building the $NAME docker image"
             sudo docker build -t bostonanalytics/${NAME} .
 EOF
 done < $ROOT/repositories.conf
