7c4a7c045b2e8a9b9d1146e56f5df9d7d14bcb96
luis sano <luissano@gmail.com>
2017-06-22 21:35:09 -0400

    that was just causing issues

--- infrastructure/codebuild/install_terraform.sh
+++ infrastructure/codebuild/install_terraform.sh
@@ -5,9 +5,9 @@ echo "Installing Terraform on Build Server"
 
 
 cd /tmp &&
- git clone https://github.com/wwalker/ssh-find-agent.git && \
- echo "source /tmp/ssh-find-agent/ssh-find-agent.bash;sett_ssh_agent_socket" >> ~/.bashrc && \
- source ~/.bashrc && \
+# git clone https://github.com/wwalker/ssh-find-agent.git && \
+# echo "source /tmp/ssh-find-agent/ssh-find-agent.bash;sett_ssh_agent_socket" >> ~/.bashrc && \
+# source ~/.bashrc && \
  curl -o terraform.zip https://releases.hashicorp.com/terraform/${TerraformVersion}/terraform_${TerraformVersion}_linux_amd64.zip && \
  echo "${TerraformSha256} terraform.zip" | sha256sum -c --quiet && \
  unzip terraform.zip && mv terraform /usr/bin
