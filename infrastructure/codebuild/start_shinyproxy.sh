36b7c87fdeda7d9132a4da25e0be61a87d21d0f7
luis sano <luissano@gmail.com>
2017-06-23 07:19:31 -0400

    this needs to go through the bastion as well

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -2,11 +2,24 @@
 # This script will kill any running shiny proxy processes and start a new one
 source /tmp/shiny_proxy_ip
 
-scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no application.yml ubuntu@$SHINY_PROXY_IP:~/shinyproxy/application.yml
-ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << 'EOF'
+#scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no application.yml ubuntu@$SHINY_PROXY_IP:~/shinyproxy/application.yml
+#ssh -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << 'EOF'
+#echo "going to kill any shinyproxy processes before starting up a new one"
+#cd ~/shinyproxy
+#echo "(jps -ml | grep shinyproxy | grep -P -o \"\\d+\\s\" | awk \"{print $1}\" | xargs kill) || echo \"nothing currently running\"; java -jar shinyproxy-0.8.7.jar" > /tmp/start_proxy.sh
+# chmod u+x /tmp/start_proxy.sh
+# nohup /tmp/start_proxy.sh  >/dev/null 2>&1 &
+#EOF
+
+scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no application.yml ec2-user@BASTION_PUBLIC_IP:/tmp/shinyproxy/application.yml
+ssh -T -A -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ec2-user@$BASTION_PUBLIC_IP << 'EOF'
+ssh -A -T -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << 'DOF'
+scp -o StrictHostKeyChecking=no /tmp/shinyproxy/application.yml ubuntu@SHINY_PROXY_IP:/tmp/shinyproxy/application.yml
 echo "going to kill any shinyproxy processes before starting up a new one"
 cd ~/shinyproxy
 echo "(jps -ml | grep shinyproxy | grep -P -o \"\\d+\\s\" | awk \"{print $1}\" | xargs kill) || echo \"nothing currently running\"; java -jar shinyproxy-0.8.7.jar" > /tmp/start_proxy.sh
- chmod u+x /tmp/start_proxy.sh
- nohup /tmp/start_proxy.sh  >/dev/null 2>&1 &
+chmod u+x /tmp/start_proxy.sh
+nohup /tmp/start_proxy.sh  >/dev/null 2>&1 &
+DOF
 EOF
+
