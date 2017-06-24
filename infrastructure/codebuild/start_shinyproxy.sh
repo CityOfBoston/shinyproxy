9b863041b4f8bb414f69e94c3465941c90937356
luis sano <luissano@gmail.com>
2017-06-24 06:29:25 -0400

    hmm

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -12,9 +12,9 @@ source /tmp/shiny_proxy_ip
 #EOF
 
 scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no application.yml ec2-user@BASTION_PUBLIC_IP:/tmp/shinyproxy/application.yml
-ssh -T -A -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ec2-user@$BASTION_PUBLIC_IP << 'EOF'
-ssh -T -o StrictHostKeyChecking=no ubuntu@$SHINY_PROXY_IP << 'DOF'
-scp -o StrictHostKeyChecking=no /tmp/shinyproxy/application.yml ubuntu@SHINY_PROXY_IP:/tmp/shinyproxy/application.yml
+ssh -T -A -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ec2-user@${BASTION_PUBLIC_IP} << EOF
+scp -o StrictHostKeyChecking=no /tmp/shinyproxy/application.yml ubuntu@${SHINY_PROXY_IP}:/tmp/shinyproxy/application.yml
+ssh -T -o StrictHostKeyChecking=no ubuntu@${SHINY_PROXY_IP} << 'DOF'
 echo "going to kill any shinyproxy processes before starting up a new one"
 cd ~/shinyproxy
 echo "(jps -ml | grep shinyproxy | grep -P -o \"\\d+\\s\" | awk \"{print $1}\" | xargs kill) || echo \"nothing currently running\"; java -jar shinyproxy-0.8.7.jar" > /tmp/start_proxy.sh
