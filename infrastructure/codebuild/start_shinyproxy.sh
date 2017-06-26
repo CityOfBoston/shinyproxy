89b628eefc04d0b4ee4ff956df0a0537e2407a3a
luis sano <luissano@gmail.com>
2017-06-26 10:16:13 -0400

    for some reason the application file is not visible on the build server

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -5,7 +5,8 @@ source /tmp/shiny_proxy_ip
 
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/shinyproxy.pem
-
+echo "why cant it find the applicaiton file"
+ls ~/
 scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ~/application.yml ec2-user@${BASTION_PUBLIC_IP}:/tmp/application.yml
 ssh -T -A -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ec2-user@${BASTION_PUBLIC_IP} << EOF
 scp -o StrictHostKeyChecking=no /tmp/application.yml ubuntu@${SHINY_PROXY_IP}:/tmp/shinyproxy/application.yml
