73f619ecafeef9f9bae6ca6be55f9afd4d7105c0
luis sano <luissano@gmail.com>
2017-06-26 21:32:18 -0400

    forgot that one

--- infrastructure/codebuild/setup_bastion.sh
+++ infrastructure/codebuild/setup_bastion.sh
@@ -3,4 +3,4 @@
 set -e
 source /tmp/shiny_proxy_ip
 eval "$(ssh-agent -s)"
-sudo scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ~/.ssh/shinyproxy.pem ubuntu@${BASTION_PUBLIC_IP}:/.ssh/shinyproxy.pem
+sudo scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ~/.ssh/shinyproxy.pem ubuntu@${BASTION_PUBLIC_IP}:~/.ssh/shinyproxy.pem
