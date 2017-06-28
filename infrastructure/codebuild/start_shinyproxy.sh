81215c7c7535fbed618bd9db8d668744732eed96
luis sano <luissano@gmail.com>
2017-06-28 16:36:59 -0400

    needed to download images manually

--- infrastructure/codebuild/start_shinyproxy.sh
+++ infrastructure/codebuild/start_shinyproxy.sh
@@ -6,10 +6,13 @@ source /tmp/shiny_proxy_ip
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/shinyproxy.pem
 scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no application.yml ubuntu@${BASTION_PUBLIC_IP}:/tmp
+scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no  /tmp/download_images.sh ubuntu@${BASTION_PUBLIC_IP}:/tmp/
 ssh -T -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no ubuntu@${BASTION_PUBLIC_IP} << EOF
 	sudo scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no /tmp/application.yml ubuntu@${SHINY_PROXY_IP}:~/shinyproxy
+	sudo scp -i ~/.ssh/shinyproxy.pem -o StrictHostKeyChecking=no /tmp/download_images.sh ubuntu@${SHINY_PROXY_IP}:/tmp/
 	ssh -i ~/.ssh/shinyproxy.pem -A -T -v -o StrictHostKeyChecking=no ubuntu@${SHINY_PROXY_IP} <<- 'DOF'
-		## Private Applications
+		# Going to pull images from ec2 repository manually
+		sudo /tmp/download_images.sh
 		echo "going to kill any shinyproxy processes before starting up a new one"
 		cd ~/shinyproxy
 		echo "(jps -ml | grep shinyproxy | grep -P -o \"\\d+\\s\" | awk \"{print $1}\" | xargs kill) || echo \"nothing currently running\"; java -jar shinyproxy-0.8.7.jar" > /tmp/start_proxy.sh
