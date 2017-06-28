8c219f09bd64cb7f6a7d9a2125ed11421e32a2c5
luis sano <luissano@gmail.com>
2017-06-28 10:07:31 -0400

    a bit of a split to the structure that I think makes more sense

---
+++ infrastructure/terraform/shiny_proxy/app_lb.tf
@@ -0,0 +1,67 @@
+
+resource "aws_alb" "frontend" {
+  name = "dev-shiny-boston"
+  internal = false
+  subnets = ["${data.aws_subnet.public.*.id}"]
+  security_groups = ["${aws_security_group.alb.id}"]
+//  access_logs {
+//    bucket = "city-of-boston"
+//    prefix = "logs/alb/dev-boston"
+//    enabled = false
+//  }
+  tags {
+    Environment = "development"
+  }
+}
+
+
+resource "aws_alb_listener" "shiny_listener" {
+  load_balancer_arn = "${aws_alb.frontend.arn}"
+  port = 80
+  protocol = "HTTP"
+  default_action {
+    target_group_arn = "${aws_alb_target_group.shiny_group.arn}"
+    type = "forward"
+  }
+}
+
+//resource "aws_alb_listener_rule" "shiny" {
+//  listener_arn = "${aws_alb_listener.shiny_listener.arn}"
+//  priority     = 1
+//
+//  action {
+//    type             = "forward"
+//    target_group_arn = "${aws_alb_target_group.shiny_group.arn}"
+//  }
+//
+//
+//  condition {
+//    field  = "path-pattern"
+//    values = ["/app/imagine-boston/*"]
+//  }
+//}
+
+
+
+resource  "aws_alb_target_group" "shiny_group" {
+  port = 8080
+  protocol = "HTTP"
+  vpc_id = "${var.vpc_id}"
+
+    health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    protocol = "HTTP"
+    path = "/login"
+    interval  = 30
+  }
+
+}
+
+resource "aws_alb_target_group_attachment" "shiny_server" {
+  target_group_arn = "${aws_alb_target_group.shiny_group.arn}"
+  target_id = "${aws_instance.shinyproxy.id}"
+}
+
+
---
+++ infrastructure/terraform/shiny_proxy/security_groups.tf
@@ -0,0 +1,80 @@
+
+resource "aws_default_security_group" "default" {
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "shinyproxy" {
+  name = "shinyproxy-security-group"
+  vpc_id = "${var.vpc_id}"
+  #Allows access from the web to this instance
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    self = true
+  }
+
+  egress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol  = "tcp"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+
+}
+
+resource "aws_security_group" "alb" {
+  name = "alb_security_group"
+  description = "Allow traffic from the internet into http ports"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    Name = "alb_security_group"
+  }
+
+}
