89f6d4b1b20820e718486eb61d5076c484d63279
luis sano <luissano@gmail.com>
2017-06-28 10:07:31 -0400

    a bit of a split to the structure that I think makes more sense

--- infrastructure/terraform/shiny_proxy/output.tf
+++ infrastructure/terraform/shiny_proxy/output.tf
@@ -9,3 +9,8 @@ output "shiny_private_ip" {
 output "alb_public_ip" {
   value = "${aws_alb.frontend.dns_name}"
 }
+
+
+output "shiny_proxy_public_ip" {
+  value = "${aws_instance.shinyproxy.public_ip}"
+}
