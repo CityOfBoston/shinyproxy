d38879f8b1f43cbb57dba45f877f9d6fc995480e
luis sano <luissano@gmail.com>
2017-07-03 10:35:30 -0400

    need to figure out where this weird timeout situation is happening

--- infrastructure/terraform/shiny_proxy/shinyproxy.tf
+++ infrastructure/terraform/shiny_proxy/shinyproxy.tf
@@ -3,86 +3,6 @@ provider "aws" {
 }
 
 
-
-//
-//resource "aws_instance" "shinyproxy" {
-//  key_name = "${var.key_name}"
-//  instance_type = "${var.instance_type}"
-//  ami = "${var.ubuntu_ami_id}"
-//  vpc_security_group_ids = ["${aws_default_security_group.default.id}", "${aws_security_group.shinyproxy.id}","${data.aws_security_group.bastion_sg.id}","${aws_security_group.alb.id}"]
-//  iam_instance_profile = "${aws_iam_instance_profile.shiny_profile.id}"
-//  subnet_id = "${element(data.aws_subnet.private.*.id, 1)}"
-//  tags {
-//    Name = "shinyproxyserver"
-//    Environment = "${var.environment}"
-//  }
-//  monitoring = true
-//  root_block_device {
-//    volume_size = 100
-//  }
-//
-//  provisioner "file" {
-//    source = "${path.module}/bin/"
-//    destination = "/tmp/"
-//    connection {
-//      type = "ssh"
-//      bastion_user = "ubuntu"
-//      bastion_host = "${data.aws_instance.bastion.public_ip}"
-//      bastion_private_key = "${file(var.ssh_key)}"
-//      bastion_port = 22
-//      agent = false
-//      user = "ubuntu"
-//      host = "${self.private_ip}"
-//      private_key = "${file(var.ssh_key)}"
-//      timeout = "5m"
-//    }
-//  }
-//  provisioner "file" {
-//    source = "${var.shiny_proxy_config_file}"
-//    destination = "/tmp/application.yml"
-//    connection {
-//      type = "ssh"
-//      bastion_user = "ubuntu"
-//      bastion_host = "${data.aws_instance.bastion.public_ip}"
-//      bastion_private_key = "${file(var.ssh_key)}"
-//      bastion_port = 22
-//      agent = false
-//      user = "ubuntu"
-//      host = "${self.private_ip}"
-//      private_key = "${file(var.ssh_key)}"
-//      timeout = "5m"
-//    }
-//  }
-//
-//  provisioner "remote-exec" {
-//    inline = [
-//      "mkdir -p ~/shinyproxy/bin",
-//      "mv /tmp/install_shiny_proxy.sh ~/shinyproxy/bin",
-//      "mv /tmp/application.yml ~/shinyproxy/application.yml",
-//      "chmod u+x ~/shinyproxy/bin/install_shiny_proxy.sh",
-//      " ~/shinyproxy/bin/install_shiny_proxy.sh"
-//
-//    ]
-//    connection {
-//      type = "ssh"
-//      bastion_user = "ubuntu"
-//      bastion_host = "${data.aws_instance.bastion.public_ip}"
-//      bastion_private_key = "${file(var.ssh_key)}"
-//      bastion_port = 22
-//      agent = false
-//      user = "ubuntu"
-//      host = "${self.private_ip}"
-//      private_key = "${file(var.ssh_key)}"
-//      timeout = "5m"
-//
-//    }
-//  }
-//
-//}
-//
-
-
-
 resource "aws_s3_bucket" "tmp" {
   bucket = "test-shiny-proxy"
   acl = "private"
@@ -93,6 +13,7 @@ resource "aws_s3_bucket_object" "application_yml" {
   bucket = "${aws_s3_bucket.tmp.id}"
   key = "/apps/${var.environment}/shinyproxy/application.yml"
   source = "${var.application_file}"
+  etag = "${md5(var.application_file)}"
 }
 
 
