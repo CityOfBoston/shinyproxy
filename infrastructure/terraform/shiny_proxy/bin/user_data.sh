2a9fa9ab65b97240834611cf0de2b75377323165
luis sano <luissano@gmail.com>
2017-07-05 17:35:52 -0400

    record image history

--- infrastructure/terraform/shiny_proxy/bin/user_data.sh
+++ infrastructure/terraform/shiny_proxy/bin/user_data.sh
@@ -41,6 +41,7 @@ sudo /tmp/login_ecr.sh
 for image in $(echo ${ecr_repositories} | sed "s/,/ /g")
 do
     docker pull "$image"
+    echo "successfully pulled $image at $${date}" >> /home/ubuntu/puled_docker_images.history
 
 done
 EOF
